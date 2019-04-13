0eddab11f3f477ab51017444cfe8f100a9857e2b
Cakaliman <43456659+Cakaliman@users.noreply.github.com>
2019-04-13 20:29:49 -0300

    Create b.c

---
+++ x_FuncoesRecursivas/Exercicio1/b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Simule para n = 5 e a[] = {2, 7, -9, 5, 6} 
+
+int main(void){
+    printf("HELLO WORLD");
+    
+    return 0;
+}
+
+int maximo (int a[], int n){
+     
+
+}
